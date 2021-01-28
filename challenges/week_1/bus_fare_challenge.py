80f3f466c6d81898524b7501f5b861f2212beb44
codebatgeek <77833198+codebatgeek@users.noreply.github.com>
2021-01-29 00:45:05 +0300

    Update bus_fare_challenge.py

--- challenges/week_1/bus_fare_challenge.py
+++ challenges/week_1/bus_fare_challenge.py
@@ -1,1 +1,23 @@
-# WRITE YOUR CODE SOLUTION HERE
+#Solution
+from datetime import date
+today = date.today()
+
+#date
+date = today.strftime("%Y-%m-%d")
+print("date:", date)
+
+#day
+day = today.strftime("%a")
+print("day:",day)
+
+#fare
+
+if today.strftime("%a") == 'Mon', 'Tue', 'Wed', 'Thu', 'Fri':
+	fare = 100
+elif today.strftime("%a") == 'Sat':
+	fare = 60
+elif today.strftime("%a") == 'Sun':
+    fare = 80
+else:
+	print("day is not valid")
+print("fare:", fare)
